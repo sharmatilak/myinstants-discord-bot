8470f9f134f5b1041ef11b830f5e3ce0f27829d6
Tilak Sharma <tilak.sharma.ai@gmail.com>
2026-07-22 14:45:57 +0530

    Add bot event to run the bot
    Also added some print statements for debugging
    (might delete them later)

--- myinstantsbot.py
+++ myinstantsbot.py
@@ -20,3 +20,15 @@ bot = commands.Bot(
     command_prefix="!",
     intents=intents
 )
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands.")
+        for cmd in synced:
+            print(cmd.name, cmd.options)
+    except Exception as e:
+        print(e)
